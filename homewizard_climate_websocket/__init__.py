c1c7426c732894e6cd5e13c3ebc0ad04395131d8
Mepla <soheil.nasirian@gmail.com>
2022-12-13 20:45:23 +0100

    Bump version 0.0.4 -> 0.0.5

--- homewizard_climate_websocket/__init__.py
+++ homewizard_climate_websocket/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Mepla"
 __email__ = "soheil.nasirian@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 
 def get_module_version():
