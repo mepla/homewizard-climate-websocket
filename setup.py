499251a5c810476c61a59e6cb1466289decd6986
Mepla <soheil.nasirian@gmail.com>
2022-12-20 18:41:44 +0100

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -88,6 +88,6 @@ setup(
     url="https://github.com/mepla/homewizard_climate_websocket",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.7",
+    version="0.0.8",
     zip_safe=False,
 )
